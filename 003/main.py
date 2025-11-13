84f2f28355415d8c310b7d336f96f0b347ae8a2c
Lennart Novak <lennart.novak@schauinsland-reisen.de>
2025-11-13 11:19:31 +0100

    feat(003): add lineedit widget

--- 003/main.py
+++ 003/main.py
@@ -1,6 +1,6 @@
 import random
 import sys
-from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout
+from PySide6.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QLineEdit
 from PySide6.QtCore import Slot, Qt
 
 class MyWidget(QWidget):
@@ -9,14 +9,21 @@ class MyWidget(QWidget):
         self.hello = ["Hallo", "Hello", "Salut", "Salam", "Ola"]
         self.button = QPushButton("Click me!")
         self.label = QLabel(self.hello[0], alignment=Qt.AlignCenter)
+        self.le = QLineEdit()
+
         self.layout = QVBoxLayout(self)
+        self.layout.addWidget(self.le)
         self.layout.addWidget(self.label)
         self.layout.addWidget(self.button)
+
         self.button.clicked.connect(self.magic)
+        self.le.returnPressed.connect(self.magic)
+        self.le.textChanged.connect(self.magic)
+
 
     @Slot()
     def magic(self):
-        self.label.setText(self.hello[random.randint(0, len(self.hello) - 1)])
+        self.label.setText(self.hello[random.randint(0, len(self.hello) - 1)] + " " + self.le.text())
 
 
 if __name__ == '__main__':
