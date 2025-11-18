788ab32501c1cc701148527b0f35a6de8c58f115
Lennart Novak <lennart.novak@schauinsland-reisen.de>
2025-11-18 10:44:26 +0100

    feat(hausaufgabe-6): finish aufgabe 6_2

--- Hausaufgaben/Hausaufgabe_6/main.py
+++ Hausaufgaben/Hausaufgabe_6/main.py
@@ -1,7 +1,15 @@
 import sys
 from PySide6.QtWidgets import QApplication, QLabel
+from PySide6.QtGui import QPixmap
+
+imgName = 'lorem_picsum'
+imgEnding = 'jpg'
+imgFileName = imgName + '.' + imgEnding
 
 app = QApplication(sys.argv)
-label = QLabel("Hello MTI!")
+pixmap = QPixmap(imgFileName)
+label = QLabel()
+label.setPixmap(pixmap)
+label.adjustSize()
 label.show()
 app.exec()
