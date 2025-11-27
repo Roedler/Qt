3cebafd1c46a7cea12238a2ce88aaa178c0edf12
Lennart Novak <lennart.novak@schauinsland-reisen.de>
2025-11-27 13:09:23 +0100

    feat(hausaufgabe-7): add Aufgaben from 7_2 to 7_6

---
+++ Hausaufgaben/Aufgabe_7/main.py
@@ -0,0 +1,54 @@
+import sys
+
+from PySide6.QtCore import Slot
+from PySide6.QtWidgets import QApplication, QMainWindow
+from compiled.Aufgabe_7 import Ui_MainWindow
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.actionSpeichern.triggered.connect(self.save)
+
+    @Slot()
+    def save(self):
+        print("save")
+
+    @Slot(int)
+    def on_horizontalSlider_valueChanged(self, value=0):
+        print("newValue ", value)
+
+        if value < 3:
+            color = "blue"
+        elif value > 20:
+            color = "red"
+        else:
+            color = "grey"
+
+        cssstr = """
+        QSlider::groove:horizontal {
+            border: 2px solid #999999;
+            height: 10px;
+            background: grey;
+            margin: 2px 0;
+        }
+        
+        QSlider::handle:horizontal {
+            background: """ + color + """;
+            border: 10px solid black;
+            width: 40px;
+            height: 40px;
+            margin: -25px 0;
+            border-radius: 25px;
+        }
+        """
+
+        self.ui.horizontalSlider.setStyleSheet(cssstr)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    widget = MainWindow()
+
+    widget.show()
+    sys.exit(app.exec())
